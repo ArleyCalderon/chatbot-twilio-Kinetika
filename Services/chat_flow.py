4f57e660e22996fe513f2dc04f10f56ecacafebb
ArleyCalderon <arleycalderon97@gmail.com>
2026-02-09 19:37:58 -0500

    se agregan logueo para diferentes usuarios

--- Services/chat_flow.py
+++ Services/chat_flow.py
@@ -278,7 +278,7 @@ def validate_and_normalize(q: dict, msg: str, data: dict):
 # region Flow menu
 MENU_TEXT = (
     
-    "Hola👋 te damos la bienvenida. Gracias por ponerte en contacto con nosotros. Antes de iniciar, es necesario que aceptes los términos y condiciones de WhatsApp.   ¿Qué deseas hacer?\n"
+    "Hola👋 te damos la bienvenida. Gracias por ponerte en contacto con nosotros. Antes de iniciar, es necesario que aceptes los términos y condiciones de WhatsApp.\n   ¿Qué deseas hacer?\n"
     "1) Agendar cita\n"
     "2) Cancelar cita\n\n"
     "Responde con 1 o 2"
